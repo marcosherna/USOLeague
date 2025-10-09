071c88809967fc6b407a4bc536720e080d3178a8
marcosherna <marcosxdf6@gmail.com>
2025-10-09 09:23:57 -0600

    feat: add icons

--- frontend/mobile/uso-league/navigations/TabNavigator.tsx
+++ frontend/mobile/uso-league/navigations/TabNavigator.tsx
@@ -1,6 +1,13 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import {
+  Home,
+  Trophy,
+  User,
+  Coffee, 
+  CirclePlus,
+} from "lucide-react-native";
 
-import HomeScreen from "../screens/HomeScreen";  
+import HomeScreen from "../screens/HomeScreen";
 import ProfileScreen from "../screens/ProfileScreen";
 import TournamentsScreen from "../screens/TournamentsScreen";
 import TeamsScreen from "../screens/TeamsScreen";
@@ -11,11 +18,43 @@ const Tab = createBottomTabNavigator();
 export default function TabNavigator() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Tournaments" component={TournamentsScreen} />
-      <Tab.Screen name="teams" component={TeamsScreen} />
-      <Tab.Screen name="Community" component={CommunityScreen} />
-      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Home color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Tournaments"
+        component={TournamentsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Trophy color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="teams"
+        component={TeamsScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => (
+            <CirclePlus color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Community"
+        component={CommunityScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <Coffee color={color} size={size} />,
+        }}
+      />
+      <Tab.Screen
+        name="Profile"
+        component={ProfileScreen}
+        options={{
+          tabBarIcon: ({ color, size }) => <User color={color} size={size} />,
+        }}
+      />
     </Tab.Navigator>
   );
 }
